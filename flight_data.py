00823aa436cc6e1c8c08ee8cb2f5da3b84b0a18b
fnamlah <faisalmalnamlah@gmail.com>
2022-11-15 16:33:25 +0300

    added modules

---
+++ flight_data.py
@@ -0,0 +1,7 @@
+class FlightData:
+    def __init__(self):
+
+    #This class is responsible for structuring the flight data.
+    pass
+
+
---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+from flight_data import FlightData
+from flight_search import FlightSearch
+from data_manager import DataManager
+from notifications_manager import NotificationManager
+"""
+message: low price alert! Only $50 usd to fly from London-STN to Berlin-SXF
+from 2022-12-20 to 2023-01-20.
+"""
+# TODO: 1. have predefined destinations and their two-ways prices (Google Sheet) (Sheety) request.get()
+# TODO: 2. fetch data from flight aggregator (request.get())
+# TODO: 3. compare prices coming from flight aggregator with the prices in (Google Sheet)
+# TODO: 4. use email to get notifications of flight deals (smtplib)
+
+
+"""
+echo "# workout-tracking" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git push -u origin main
+"""
